40d3663251a7cd5ff7e60b1dcb5b1e766dc47408
iStealthStore <103111783+iStealthStore@users.noreply.github.com>
2022-04-07 11:08:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged In as ${client.user.username}#${client.user.discriminator}`)
-    client.user.setActivity('SELL NITRO BOOST 25K', { type: 'PLAYING' })
+    client.setActivity('SELL NITRO BOOST 25K', { type: 'PLAYING' })
 })
 
 client.login(process.env.TOKEN2)
